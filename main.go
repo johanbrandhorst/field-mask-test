bf30bb4984a103ef2702860303d2063e3760e454
Johan Brandhorst <johan.brandhorst@gmail.com>
2018-03-27 22:33:00 +0100

    Add main file

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"net/http"
+	"os"
+
+	"github.com/gogo/status"
+	"github.com/grpc-ecosystem/grpc-gateway/runtime"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/grpclog"
+
+	pbExample "github.com/johanbrandhorst/grpc-example/proto"
+)
+
+var (
+	gRPCPort    = flag.Int("grpc-port", 10000, "The gRPC server port")
+	gatewayPort = flag.Int("gateway-port", 11000, "The gRPC-Gateway server port")
+)
+
+//go:generate protoc -I. -I ./vendor/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:./ --grpc-gateway_out=:./ ./proto/test.proto
+
+var log grpclog.LoggerV2
+
+func init() {
+	log = grpclog.NewLoggerV2(os.Stdout, os.Stderr, os.Stderr)
+	grpclog.SetLoggerV2(log)
+}
+
+func main() {
+	flag.Parse()
+	addr := fmt.Sprintf("localhost:%d", *gRPCPort)
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalln("Failed to listen:", err)
+	}
+	s := grpc.NewServer()
+	pbExample.RegisterTestFieldMaskServer(s, &srv{})
+
+	// Serve gRPC Server
+	log.Info("Serving gRPC on https://", addr)
+	go func() {
+		log.Fatal(s.Serve(lis))
+	}()
+
+	dialAddr := fmt.Sprintf("ipv4://localhost/%s", addr)
+	conn, err := grpc.DialContext(
+		context.Background(),
+		dialAddr,
+		grpc.WithInsecure(),
+	)
+	if err != nil {
+		log.Fatalln("Failed to dial server:", err)
+	}
+
+	gwmux := runtime.NewServeMux()
+	err = pbExample.RegisterTestFieldMaskHandler(context.Background(), gwmux, conn)
+	if err != nil {
+		log.Fatalln("Failed to register gateway:", err)
+	}
+
+	gatewayAddr := fmt.Sprintf("localhost:%d", *gatewayPort)
+	log.Info("Serving gRPC-Gateway on https://", gatewayAddr)
+	gwServer := http.Server{
+		Addr:    gatewayAddr,
+		Handler: gwmux,
+	}
+	log.Fatalln(gwServer.ListenAndServe())
+}
+
+type srv struct{}
+
+func (s *srv) UpdateUser(ctx context.Context, req *pbExample.UpdateUserRequest) (*pbExample.User, error) {
+	fmt.Println(req.GetUpdateMask().GetPaths())
+	return nil, status.Error(codes.NotFound, "user was not found")
+}
